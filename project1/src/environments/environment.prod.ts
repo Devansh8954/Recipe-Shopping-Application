3e1971e765791731e6010c74367eb5914b8d5f69
DEVANSH TYAGI <96236567+Devansh8954@users.noreply.github.com>
2024-01-07 18:03:21 +0530

    updated

--- project1/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    firebaseAPIKey: 'AIzaSyBnAuz8wrE403RjnhfEnnN35wPrlHGqIeM'
-}
--- project1/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    firebaseAPIKey: 'AIzaSyBnAuz8wrE403RjnhfEnnN35wPrlHGqIeM'
-}
